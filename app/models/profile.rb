d2b8e95168e734001b02073a1aec7a579542e93d
Andrea Miranda <amiranda@buk.pe>
2022-10-03 09:52:13 -0500

    feat: add username validations in profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,9 @@
 class Profile < ApplicationRecord
+
+  #VALIDATIONS
+  validates :username,  
+            presence: true,
+            length: {maximum: 255},
+            uniqueness: true,
+            format: { with: /\A(?=.*[a-z])[a-z-\d]+\Z/i }
 end
