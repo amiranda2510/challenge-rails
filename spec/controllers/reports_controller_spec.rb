8ceb86d1efee6586a4d1513dd081ce695dbcbfc5
Andrea Miranda <amiranda@buk.pe>
2022-10-05 14:56:01 -0500

    test: test for reports controller

---
+++ spec/controllers/reports_controller_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe ReportsController, type: :controller do
+  before(:each) do
+    @profile = Profile.create!({ username: 'mockito' })
+    @profile_two = Profile.create!({ username: 'mockito-2' })
+  end
+
+  before(:each) do
+    @repository = Repository.create!({ name: 'mockito repo', tags: 'repo', profile_id: @profile.id })
+    @repository_two = Repository.create!({ name: 'mockito repo 2', tags: 'lala', profile_id: @profile_two.id })
+  end
+
+  let(:valid_params) do
+    { name: 'Test title', director: 'Test', release_date: '01/20/2015', category_id: @category.id,
+      status: 1 }
+  end
+
+  describe 'GET #index' do
+    it 'renders index' do
+      get :index, params: {}
+
+      expect(response).to be_successful
+      expect(response).to render_template(:index)
+    end
+
+    it 'returns all profiles with their repos' do
+      get :index, params: {}, :format => :json
+
+      expect(response.body).to include('mockito')
+      expect(response.body).to include('mockito-2')
+      expect(response.body).to include('mockito repo')
+      expect(response.body).to include('mockito repo 2')
+    end
+
+    it 'renders filtered profiles' do
+      get :index, params: { tag: 'repo'}, :format => :json
+
+      expect(response.body).to include('mockito repo')
+      expect(response.body).to_not include('lala')
+    end
+  end
+end
